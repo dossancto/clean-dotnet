c6c7733d026a72f1173b1c5c3946a6ced579fdc1
lu-css <dossantossilvalucas963@gmail.com>
2023-09-22 23:00:09 -0300

    refactor: change project type to webapi

--- Limpa/Program.cs
+++ Limpa/Program.cs
@@ -1,10 +1,25 @@
-﻿using Limpa.Domain.Entities;
+var builder = WebApplication.CreateBuilder(args);
 
-internal class Program
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var user = new User("Lucas", 18);
-        Console.WriteLine(user);
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
